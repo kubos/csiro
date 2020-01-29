466720ec4edcc02c7f19fd70211493d515998947
Ryan Plauche <ryan@kubos.co>
2020-01-29 08:15:50 -0600

    Switched back to original emcraft toolchain. Added +strict-align feature to fix unaligned access faults. Turned off panic aborts and enabled building libpanic_unwind. Changed main to test a bit more of libstd.

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    std::process::exit(5);
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufWriter;
+
+fn calc_return() -> i32 {
+    let num = 10;
+    let second = 6;
+    num + second
+}
+
+fn test_vars() {
+    let num = 10;
+    let vec = vec![1,2,3,4];
+    let name = "Ryan";
+    let alive = true;
+}
+
+pub fn main() {
+    test_vars();
+    let f = File::create("hi.txt").unwrap();
+    {
+        let mut writer = BufWriter::new(f);
+        writer.write("Hello".as_bytes()).unwrap();
+    }
+    println!("Hello Rusty Cortex-M3");
+    std::process::exit(calc_return());
 }
